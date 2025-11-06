609dca2a678d5deabf3249d09bf18896d47e0bfb
MrHarris <haritsbusinessacc@gmail.com>
2025-11-07 02:26:32 +0800

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  // ADD THIS LINE TO FIX THE DEPLOYMENT PATH ERROR:
-  base: './', 
-  plugins: [react()],
-})
